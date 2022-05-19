ff19f26790cd1bc450a894380f136fe6cabff27b
Laura Lindzey <lindzey@uw.edu>
2022-05-19 00:25:23 -1000

    Clean up SMS parsing; add support for changing rates

--- nodes/modem_node.py
+++ nodes/modem_node.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import binascii
+import re
 import rospy
+from typing import Dict, List, Optional, Tuple
 
 from std_msgs.msg import String
 from sonardyne_msgs.msg import SMS
@@ -10,35 +11,34 @@ from diagnostic_msgs.msg import DiagnosticArray
 from diagnostic_msgs.msg import DiagnosticStatus
 from diagnostic_msgs.msg import KeyValue
 
-from ros_acomms.msg import ReceivedPacket
-from ros_acomms.srv import QueueTxPacket, QueueTxPacketResponse
+from ros_acomms.msg import Packet, ReceivedPacket
+from ros_acomms.srv import QueueTxPacket, QueueTxPacketRequest, QueueTxPacketResponse
 
 import sonardyne_usbl.modem
 
 
 class SonardyneModemNode(object):
-    def __init__(self):
-        connection_type = rospy.get_param('~connection/type')
-        if connection_type == 'serial':
-            port = rospy.get_param('~connection/port')
-            baud_rate = rospy.get_param('~connection/baud_rate', 9600)
-            connection = sonardyne_usbl.modem.SerialConnection(port, baud_rate)
-        elif connection_type == 'tcp':
-            host = rospy.get_param('~connection/host')
-            port = rospy.get_param('~connection/port', 4000)
-            connection = sonardyne_usbl.modem.TCPConnection(host, port)
-        elif connection_type == 'udp':
-            host = rospy.get_param('~connection/host')
-            input_port = rospy.get_param('~connection/input_port', 50011)
-            output_port = rospy.get_param('~connection/outport_port', 50010)
-            connection = sonardyne_usbl.modem.UDPConnection(input_port, output_port, host)
+    def __init__(self) -> None:
+        # Sonardyne Telemetry Schemes; not all modems support all of them.
+        self.valid_rates = [0,1,2,3,4,5,6,7,8]
+        self.default_rate = int(rospy.get_param("~default_rate"))
+        if self.default_rate not in self.valid_rates:
+            msg = "{} is not a valid sonardyine Telemetry Scheme"
+            raise Exception(msg)
 
-        self.modem = sonardyne_usbl.modem.Modem(connection)
+        try:
+            self.sdyne_addr = rospy.get_param("~sdyne_addr")
+        except KeyError:
+            msg = "sdyne_addr is a required parameter"
+            raise Exception(msg)
+        except ValueError:
+            msg = "sdyne_addr must be an int"
+            raise Exception(msg)
+
+        self.setupConnection()
+        self.modem = sonardyne_usbl.modem.Modem(self.connection)
+
         self.modem.enableDiagnostics()
-
-        self.SRC = rospy.get_param('~SRC', 2501)
-        self.DEST = rospy.get_param('~DEST', 2105)
-        self.incoming_string = ""
 
         self.message_pub = rospy.Publisher('~received_sms', SMS, queue_size=10)
         self.raw_pub = rospy.Publisher('~raw', String, queue_size=10)
@@ -49,7 +49,7 @@ class SonardyneModemNode(object):
         self.diagnostic_pub = rospy.Publisher('~diagnostics', DiagnosticArray, queue_size=10)
 
         # ros_acomms specific setup
-        self.packet_rx_pub = rospy.Publisher('~packet_rx', ReceivedPacket,
+        self.packet_rx_pub = rospy.Publisher('packet_rx', ReceivedPacket,
                                              queue_size=10)
 
 
@@ -58,11 +58,30 @@ class SonardyneModemNode(object):
 
         timer = rospy.Timer(rospy.Duration(0.1), self.timerCallback)
 
-    def timerCallback(self, event):
+    def setupConnection(self) -> None:
+        connection_type = rospy.get_param('~connection/type')
+        if connection_type == 'serial':
+            port = rospy.get_param('~connection/port')
+            baud_rate = rospy.get_param('~connection/baud_rate', 9600)
+            self.connection = sonardyne_usbl.modem.SerialConnection(port, baud_rate)
+        elif connection_type == 'tcp':
+            host = rospy.get_param('~connection/host')
+            port = rospy.get_param('~connection/port')
+            self.connection = sonardyne_usbl.modem.TCPConnection(host, port)
+        elif connection_type == 'udp':
+            remote_host = rospy.get_param('~connection/remote_host')
+            input_port = rospy.get_param('~connection/input_port')
+            output_port = rospy.get_param('~connection/output_port')
+            self.connection = sonardyne_usbl.modem.UDPConnection(input_port, output_port, remote_host)
+
+    def timerCallback(self, _event: rospy.timer.TimerEvent) -> None:
         data = self.modem.getResponse()
         if data is not None:
-            print(data)
-            self.modem_rx_handler(data)
+            # The getResponse parsing is a bit complicated; the ros_acomms side of
+            # things just needs raw byes, so we just do our own parsing.
+            if "SMS" in data['raw']:
+                self.publishReceivedPacket(data['raw'])
+
             self.raw_pub.publish(String(data['raw']))
             if 'response_type' in data and data['response_type'] == 'SMS' and 'message' in data:
                 sms = SMS()
@@ -81,72 +100,115 @@ class SonardyneModemNode(object):
                 diag_array.status.append(ds)
                 self.diagnostic_pub.publish(diag_array)
 
-    def sendSMSCallback(self, msg):
+    def sendSMSCallback(self, msg: SMS) -> None:
         self.modem.sendSMS(msg.address, msg.message)
 
-    def sendRawCallback(self, msg):
+    def sendRawCallback(self, msg: String) -> None:
         self.modem.send(msg.data+'\n')
 
-    def pack_data(self, data):
-        packet = data.hex().encode('ascii')
-        return packet
+    def parseSMS(self, msg: str) -> Tuple[Optional[int], Optional[bytes]]:
+        # NOTE(lindzey): I think you can compile the regex ahead of time for
+        #   some sort of performance improvement? (Not like this is high-rate...)
+        regex = r"^(?P<prelim>.*)SMS:(?P<addr>[0-9]{4})(?P<params>.*)\|(?P<data>[0-9A-F]*)"
+        mm = re.match(regex, msg)
+        if mm is None:
+            print("Could not parse SMS data from reply: {}".format(msg))
+            return None, None
+        addr = int(mm['addr'])  # Should succeed, since regex matched to 4 digits
+        data = mm['data']
+        try:
+            # Given that the regex matches hex characters, this will only fail
+            # if it's an odd-length string.
+            data_bytes = bytes.fromhex(data)
+        except ValueError:
+            rospy.logerr("Could not convert {} chars to hex: {!r}"
+                         .format(len(data_bytes), data_bytes))
+            return addr, None
+        print("Received {} bytes from {}".format(len(data_bytes), addr))
+        return addr, data_bytes
 
-    def scan_data(self, incoming_hex_string: str):
-        rospy.logwarn("scan_data: Got {} new bytes".format(len(incoming_hex_string)))
-        hex_chars = '0123456789abcdefABCDEF'
-        new_hex_bytes = incoming_hex_string
-        rospy.loginfo("new hex bytes: {}".format(new_hex_bytes))
+    def publishReceivedPacket(self, raw_data: str) -> None:
+        # NOTE(lindzey): This might be where we add a state machine checking
+        #    whether a given message was sent successfully.
+        if 'FAILED' in raw_data:
+            rospy.loginfo("SMS FAILED: {}".format(raw_data))
+            return
+        elif 'NO_REPLY' in raw_data:
+            # Only applies if messages sent with A0 or A1
+            rospy.loginfo("No reply to SMS. {}".format(raw_data))
+            return
+        elif 'ACK' in raw_data:
+            rospy.loginfo("Received ACK. {}".format(raw_data))
+            return
+        elif 'OK' in raw_data:
+            rospy.loginfo("Transmission confirmed: {}".format(raw_data))
+            return
 
-        # Remove non-hex bytes and any remnants of str<->bytes conversion
-        updated_hex_bytes = new_hex_bytes[1:int(len(new_hex_bytes))]
-        rospy.logwarn("updated_hex_bytes: {}".format(updated_hex_bytes))
-        clean_hex_bytes = "".join([c for c in updated_hex_bytes if c in hex_chars])
+        print("data is {}".format(raw_data))
+        src_addr, data_bytes = self.parseSMS(raw_data)
+        if data_bytes is None:
+            rospy.logwarn("Could not parse SMS: {}".format(raw_data))
+            return
 
-        self.incoming_string += clean_hex_bytes
-        return self.incoming_string
+        packet = ReceivedPacket()
+        packet.header.stamp = rospy.Time.now()
+        packet.packet.src = src_addr
+        packet.packet.dest = self.sdyne_addr  # TODO(lindzey): Add this to class?
+        packet.packet.packet_type = Packet.PACKET_TYPE_UNKNOWN
+        # Not sure how else to say "n/a"
+        packet.packet.miniframe_rate = -1
+        # The Sonardyne actually changes rates, but the received message
+        # doesn't indicate what rate it was received on.
+        packet.packet.dataframe_rate = -1
+        # We don't have miniframes / dataframes, and ros_acomms assumes that
+        # there will be data in the miniframes, so only using those.
+        packet.packet.miniframe_bytes = data_bytes
+        packet.packet.dataframe_bytes = []
+        packet.minibytes_valid = [0, len(data_bytes)]
+        packet.databytes_valid = []
+        # The CST is micromodem specific, so don't fill that out.
+        self.packet_rx_pub.publish(packet)
 
-    def modem_rx_handler(self, data):
-        rxpacket = ReceivedPacket()
-        rxpacket.header.stamp = rospy.Time.now()
-        if data['response_type'] == 'SMS':
-            if 'FAILED' in data['raw']:
-                rospy.logerr("SMS FAILED, cannot parse")
-                return
-            else:
-                print("data is {}".format(data['raw']))
-                addrSplit = data['raw'].split('[')
-                srcAddr = ''.join(x for x in addrSplit[0] if x.isdigit())
-                rxpacket.packet.src = int(srcAddr)
+    def handleQueueTxPacket(self, request: QueueTxPacketRequest) -> None:
+        # NOTE(lindzey): This driver does not implement any sort of queue,
+        #    and simply attempts to send a message as soon as a request is
+        #    received. Unlike the Micromodem, we don't get a "transmission
+        #    finished" message, so any sort of queueing would be based on
+        #    a lookup table for rates + num_bytes -> transmission times.
+        #  request.queue and request.insert_at_head are ignore
 
-                if 'NO_REPLY' in data['raw']:
-                    rospy.logwarn("rx no reply from {}".format(rxpacket.packet.src))
-                    return
-                elif 'ACK' in data['raw']:
-                    rospy.logwarn("RX ACK from {}".format(rxpacket.packet.src))
-                    return
-                else:
-                    msgSplit = data['raw'].split('|') #split on | as per sdyne docs
-                    rxMsg = self.scan_data(msgSplit[1]) #parse hex chars in SMS msg
-                    rxMsgBytes = binascii.unhexlify(rxMsg) # unhex to get rx bytes
-                    rxpacket.packet.miniframe_bytes = rxMsgBytes
-                    rxpacket.cst.src = rxpacket.packet.src
-                    self.packet_rx_pub.publish(rxpacket)
+        # TODO(lindzey): Look into whether we can adjust the transmit power.
+        # For now, simply ignore request.requested_src_level_db
 
-
-    def handleQueueTxPacket(self, request):
-        rospy.loginfo(
-            "handle_queue_tx_packet: Queueing new packet for transmission: {}".format(request))
-        queue_tx_packet_resp = QueueTxPacketResponse()
-        dest_address = str(request.packet.dest)
-        packetToSend = self.pack_data(request.packet.miniframe_bytes)
-
-        self.modem.sendSMS(dest_address, str(packetToSend))
-        if request.insert_at_head:
-            queue_tx_packet_resp.position_in_queue = 0
-            queue_tx_packet_resp.success = True
+        if request.packet.dataframe_rate not in self.valid_rates:
+            rospy.logerr_once("Requested invalid rate {} for Sonardyne"
+                              .format(request.packet.dataframe_rate))
+            rate = self.default_rate
         else:
-            queue_tx_packet_resp.success = True
-        return queue_tx_packet_resp
+            rate = request.packet.dataframe_rate
+
+        if len(request.packet.dataframe_bytes) > 0:
+            rospy.logerr("Sonardyne only supports a single class of data, "
+                         "but dataframe bytes were provided.")
+            data = request.packet.miniframe_bytes + request.packet.dataframe_bytes
+        else:
+            data = request.packet.miniframe_bytes
+        # Sonardyne requires uppercase in order to use 4-bit encoding of
+        # the message chars.
+        hex_data = data.hex().upper()
+
+        # The Modem class's sendSMS isn't flexible enough, so roll our own.
+        sms_cmd = "SMS:{:04d};TS{},P0,C0,A3,R0,B0|{}\n".format(request.packet.dest, rate, hex_data)
+        rospy.logerr("Sending command!: {}".format(sms_cmd))
+        self.modem.send(sms_cmd)
+
+        response = QueueTxPacketResponse()
+        # We don't get immediate feedback about success in this thread.
+        # So, for simplicity, always report true. The alternative is to
+        # wait for a SMS:OK or SMS:FAILED, but I'm not sure how to store
+        # this service call to match with data from another thread.
+        response.success = True
+        return response
 
 
 if __name__ == '__main__':
--- src/sonardyne_usbl/modem.py
+++ src/sonardyne_usbl/modem.py
@@ -3,97 +3,112 @@
 import socket
 import serial
 import time
+from typing import Any, Dict, Optional, Union
 
-class TCPConnection:
-    def __init__(self, host, port=4000):
+# TODO: mimic the ds_asio publication of all RawData to/from the port?
+
+class TCPConnection(object):
+    def __init__(self, host: str, port: int = 4000) -> None:
         self.host = host
         self.port = port
         self.open()
 
-
-    def open(self):
+    def open(self) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(0.1)
         self.sock.connect((self.host, self.port))
 
-    def send(self, data):
+    def send(self, data: str) -> None:
         self.sock.send(data.encode('utf-8'))
 
-    def recv(self):
+    # QUESTION(lindzey): Why convert away from bytes so early?
+    #    sending binary data from SMS is just going to re-encode it.
+    def recv(self) -> str:
         return self.sock.recv(10000).decode('utf-8')
 
-class UDPConnection:
-    def __init__(self, inport, outport, remote_host, local_address=''):
+class UDPConnection(object):
+    def __init__(self,
+                 inport: int,
+                 outport: int,
+                 remote_host: str,
+                 local_address: str = '') -> None:
         self.inport = inport
         self.local_address = local_address
         self.remote_address = (remote_host, outport)
         self.open()
 
-
-    def open(self):
+    def open(self) -> None:
         self.in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.in_socket.bind((self.local_address, self.inport))
         self.in_socket.settimeout(0.1)
 
         self.out_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def send(self, msg):
+    def send(self, msg: str) -> None:
         self.out_socket.sendto(msg.encode('utf-8'), self.remote_address)
 
-    def recv(self):
+    # QUESTION: Same issue as before ... why decode to a string?
+    def recv(self) -> Optional[str]:
         try:
             return self.in_socket.recv(10000).decode('utf-8')
         except socket.timeout:
             pass
         return None
 
-class SerialConnection:
-    def __init__(self, device, baud=9600):
+class SerialConnection(object):
+    def __init__(self, device: str, baud:int = 9600) -> None:
         self.device = device
         self.baud = baud
         self.open()
 
-    def open(self):
+    def open(self) -> None:
         self.port = serial.Serial(self.device, self.baud)
         self.port.timeout = 0.1
 
-    def send(self, data):
+    def send(self, data: str) -> None:
         self.port.write(data.encode('utf-8'))
-        #return self.recv()
 
-    def recv(self):
+    def recv(self) -> Optional[str]:
         data = self.port.readline().decode('utf-8')
         if len(data):
             return data
         return None
 
-class Modem:
-    def __init__(self, connection):
+class Modem(object):
+    def __init__(self,
+                 connection: Union[SerialConnection, UDPConnection, TCPConnection]
+                ) -> None:
         self.connection = connection
 
-    def send(self, data):
+    def send(self, data: str) -> None:
         print('sending:',data)
         self.connection.send(data)
 
-    def recv(self):
+    def recv(self) -> Optional[str]:
         return self.connection.recv()
 
-    def enableDiagnostics(self):
+    def enableDiagnostics(self) -> None:
         #self.send('DIAG:XC,DBV,SNR,FEC,TEL,DOP,RX1,IFL\n')
         self.send('DIAG:XC,DBV,SNR,IFL\n')
 
-    def disableDiagnostics(self):
+    def disableDiagnostics(self) -> None:
         self.send('DIAG:NONE\n')
 
-    def hardwareSelfTest(self):
+    def hardwareSelfTest(self) -> None:
         self.send('CKHW\n')
 
-    def getStatus(self, cmd, address=None):
+    def getStatus(self, cmd: str, address: Optional[int] = None) -> None:
         send_cmd = cmd
         if address is not None:
-            send_cmd += ':'+str(address)
+            send_cmd += ':{:04d}'.format(address)
         self.send(send_cmd+'\n')
 
+    # NOTE(lindzey): I'd love to see a better return data type =)
+    #  I tried to annotate it for mypy, but have failed to make it error-free.
+    #  * Most fields are strings, some of which can be None (response_type)
+    #  * Some are bool ('external_power', 'tilt_over_45', 'override_flag')
+    #  * Then, we have the battery dict of its own: List[Dict[str, Union[bool, str]]],
+    #  * Finally, the return of ParseDiag: Dict[str,str]
     def getResponse(self):
         raw = self.recv()
         if raw is None:
@@ -113,7 +128,6 @@ class Modem:
         if '[' in data:
             data,diag = data.strip().split('[',1)
 
-
         response_type = None
         UID = None
 
@@ -129,6 +143,7 @@ class Modem:
                         UID = part
                     else:
                         response_type = part.split(':',1)[0]
+
                 if response_type == 'FS':
                     ret['response_type'] = 'fixed_status'
                 elif response_type == 'CS':
@@ -139,8 +154,10 @@ class Modem:
                     ret['response_type'] = 'volatile_status'
                 else:
                     ret['response_type'] = response_type
+
                 if response_type in ('FS','CS','MS','VS','SMS'):
                     ret['address'] = part.split(':',1)[1]
+
                 if response_type == 'CKHW':
                     ret['test_result'] = part.split(':',1)[1]
 
@@ -212,7 +229,7 @@ class Modem:
                 if part == 'OV':
                     ret['override_flag'] = True
                 if len(part) > 2 and part[:2] == 'BT':
-                    battery = {}
+                    battery: Dict[str, Union[bool, str]] = {}
                     bparts = part.split(';')
                     if len(bparts) > 1:
                         battery['type'] = bparts[1]
@@ -234,10 +251,11 @@ class Modem:
                         if bpart == 'CHG':
                             battery['charging'] = True
 
-                    if not 'batteries' in ret:
+                    if  'batteries' not in ret:
                         ret['batteries'] = []
+                    # Mypy doesn't like this, because it can't tell that this entry
+                    # is guaranteed to be a list.
                     ret['batteries'].append(battery)
-
 
         if diag is not None:
             ret['diag'] = self.parseDiag(diag[:-1])
@@ -247,9 +265,13 @@ class Modem:
 
         ret['raw'] = raw
 
+        # QUESTION(lindzey): Where does error detection and tracking come in?
+        #    e.g. detecting a failed SMS? Right now, it's in Isaac's
+        #    modifications, but I think that it would make more sense to put
+        #    here.
         return ret
 
-    def parseDiag(self, diag):
+    def parseDiag(self, diag: str) -> Dict[str, str]:
         ret = {}
         parts = diag.split(',')
         for part in parts:
@@ -259,49 +281,84 @@ class Modem:
                 ret['signal_to_noise_ratio'] = part[3:]
             if part.startswith('DBV'):
                 ret['decibel_volts'] = part[3:]
-            
+
         ret['raw'] = diag
         return ret
 
-    def fixedStatus(self, address=None):
+    def fixedStatus(self, address: Optional[int] = None) -> None:
         self.getStatus('FS', address)
 
-    def configStatus(self, address=None):
+    def configStatus(self, address:int = None) -> None:
         self.getStatus('CS', address)
 
-    def volatileStatus(self, address=None):
+    def volatileStatus(self, address: Optional[int] = None) -> None:
         self.getStatus('VS', address)
 
-    def sendMessage(self, address, data):
-        self.connection.send('MDFT:'+address+'|'+data+'\n')
+    def sendMessage(self, address: int, data: str) -> None:
+        msg = "MDFT:{:04d}|{}\n".format(address, data)
+        self.connection.send(msg)
 
-    def sendSMS(self, address, data):
-        self.connection.send('SMS:'+address+',R1|'+data+'\n')
+    # NOTE(lindzey): This needs additional parameters:
+    #    * ";TS#"  -- telemetry scheme
+    #    * ",A#"  -- A3 for no reply, A0 for buffer, A1 for ack-only
+    #    * ",R#"  -- only request ranges for data with reply (A0 or A1)
+    #    * address really should be an int ...
+    def sendSMS(self, address: str, data: str) -> None:
+        msg = "SMS:{},R1|{}\n".format(address, data)
+        self.connection.send(msg)
 
-    def modemStatus(self):
+    def modemStatus(self) -> None:
         self.getStatus('MS')
 
+
 if __name__ == '__main__':
-    import sys
-    if sys.argv[1] == 'serial':
-        c = SerialConnection(sys.argv[2])
-    if sys.argv[1] == 'tcp':
-        c = TCPConnection(sys.argv[2])
-    m = Modem(c)
-    m.hardwareSelfTest()
-    m.enableDiagnostics()
-    address = None
-    if len(sys.argv) > 3:
-        address = sys.argv[3]
-    m.fixedStatus(address)
-    m.configStatus(address)
-    m.volatileStatus(address)
-    m.modemStatus()
-    if len(sys.argv) > 4:
-        m.sendSMS(sys.argv[3], sys.argv[4])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("connection_type", type=str, help="Type of connection: serial, udp, tcp")
+    parser.add_argument("--addr", type=int, help="This modem's address; used for status queries")
+    parser.add_argument("--inport", type=int, help="UDP port to listen on")
+    parser.add_argument("--outport", type=int, help="UDP port to send commands to")
+    parser.add_argument("--remote_host", type=str, help="address to send commands to")
+    parser.add_argument("--tcp_host", type=str, help="host for TCP connection")
+    parser.add_argument("--device", type=str, help="Serial device (e.g. /dev/ttyUSB0)")
+    parser.add_argument("--dest_addr", type=str, help="Address to send SMS to")
+    parser.add_argument("--sms", type=str, help="Content of SMS message to send.")
+
+    args = parser.parse_args()
+
+    # Declaring this to make mypy happy
+    connection: Union[SerialConnection, TCPConnection, UDPConnection]
+    if args.connection_type == 'serial':
+        connection = SerialConnection(args.device)
+    elif args.connection_type == 'tcp':
+        connection = TCPConnection(args.tcp_host)
+    elif args.connection_type == 'udp':
+        if args.inport is None:
+            print("ERROR: a UDP connection requires specifying 'inport'")
+        if args.outport is None:
+            print("ERROR: a UDP connection requires specifying 'inport'")
+        if args.remote_host is None:
+            print("ERROR: a UDP connection requires specifying 'inport'")
+        if args.inport and args.outport and args.remote_host:
+            connection = UDPConnection(args.inport, args.outport, args.remote_host)
+    else:
+        print("ERROR: Must specify a connection type! (udp, tcp, serial)")
+
+    modem = Modem(connection)
+    modem.hardwareSelfTest()
+
+    modem.enableDiagnostics()
+    if args.addr is not None:
+        modem.fixedStatus(args.address)
+        modem.configStatus(args.address)
+        modem.volatileStatus(args.address)
+    modem.modemStatus()
+    # NOTE(lindzey): This no longer works with UDP.
+    if args.dest_addr is not None and args.sms is not None:
+        modem.sendSMS(args.dest_addr, args.sms)
 
     while True:
-        data = m.getResponse()
+        data = modem.getResponse()
         if data is not None and len(data):
             print (data)
         else:
